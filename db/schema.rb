2b6d1f3e36585a310c7a234d43b2e3649677cc78
Evgeny Bezlyudnyy <bezlyudnyyevgeny@gmail.com>
2024-11-19 16:23:58 +0100

    added the flat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_19_143221) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_151420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.string "title"
+    t.integer "price"
+    t.integer "status"
+    t.integer "rooms"
+    t.string "adress"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_143221) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "flats", "users"
 end
